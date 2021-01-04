2b2748528198010e61ab7f9495d9b71a1b867fe8
bipulpoudel <59637626+bipulpoudel@users.noreply.github.com>
2021-01-04 21:42:53 +0545

    [updated] cart page updated

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -1,17 +1,64 @@
 import React from "react";
-
+import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+
+//icon imports
+import AddIcon from "@material-ui/icons/Add";
+import RemoveIcon from "@material-ui/icons/Remove";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+  image: { height: 100, marginBottom: 5, width: 100 },
+}));
 
 const CartItem = () => {
+  const classes = useStyles();
+
   return (
-    <Grid container spacing={1}>
-      <Grid item md={3}>
-        <img src="https://source.unsplash.com/random" style={{ height: 100 }} />
+    <>
+      <Grid container spacing={1} className={classes.root}>
+        <Grid item md={3}>
+          <img
+            src="https://source.unsplash.com/random"
+            className={classes.image}
+            alt="Product"
+          />
+          <ButtonGroup
+            size="small"
+            color="secondary"
+            variant="contained"
+            aria-label="small outlined button group"
+          >
+            <Button startIcon={<AddIcon />} />
+            <Button>2</Button>
+            <Button startIcon={<RemoveIcon />} />
+          </ButtonGroup>
+        </Grid>
+        <Grid item md={9}>
+          <Typography variant="h6" gutterBottom>
+            Product 1
+          </Typography>
+          <Typography variant="caption" display="block" gutterBottom>
+            Category
+          </Typography>
+          <Typography variant="button" display="block" gutterBottom>
+            Shop Name
+          </Typography>
+          <Typography variant="subtitle2" gutterBottom>
+            Rs 199 /-
+          </Typography>
+
+          <Button color="secondary">REMOVE</Button>
+        </Grid>
       </Grid>
-      <Grid item md={9}>
-        Product Name Details all will be here
-      </Grid>
-    </Grid>
+      <Divider />
+    </>
   );
 };
 
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -4,8 +4,23 @@ import Grid from "@material-ui/core/Grid";
 import CardHeader from "@material-ui/core/CardHeader";
 import Divider from "@material-ui/core/Divider";
 import CartItem from "../components/cart/CartItem";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+
+const useStyles = makeStyles((theme) => ({
+  placeOrderButton: {
+    marginLeft: "auto",
+  },
+}));
 
 const CartPage = () => {
+  const classes = useStyles();
   return (
     <Grid container spacing={3}>
       <Grid item xs={12} lg={8} md={6}>
@@ -15,7 +30,12 @@ const CartPage = () => {
             titleTypographyProps={{ variant: "button" }}
           />
           <Divider />
-          <CartItem />
+          <CardContent>
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <CartItem />
+          </CardContent>
         </Card>
       </Grid>
 
@@ -26,7 +46,39 @@ const CartPage = () => {
             titleTypographyProps={{ variant: "button" }}
           />
           <Divider />
-          Hello
+          <CardContent>
+            <List dense={true}>
+              <ListItem>
+                <ListItemText primary="Single-line item" />
+                <ListItemSecondaryAction>
+                  <p>Test</p>
+                </ListItemSecondaryAction>
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Single-line item" />
+                <ListItemSecondaryAction>
+                  <p>Test</p>
+                </ListItemSecondaryAction>
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Single-line item" />
+                <ListItemSecondaryAction>
+                  <p>Test</p>
+                </ListItemSecondaryAction>
+              </ListItem>
+            </List>
+          </CardContent>
+          <Divider />
+          <CardActions>
+            <Button
+              variant="contained"
+              color="secondary"
+              size="large"
+              className={classes.placeOrderButton}
+            >
+              Place Order
+            </Button>
+          </CardActions>
         </Card>
       </Grid>
     </Grid>
